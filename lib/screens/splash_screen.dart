328d0e72f270f9898622276214691d7ffcea2dfa
alessandragaleno <alesandragaleno421@gmail.com>
2024-10-23 20:00:54 -0300

    main

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -17,11 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
       backgroundColor: Color.fromARGB(255, 80, 184, 248), // cor de fundo da splash
-=======
-      backgroundColor: Color.fromARGB(255, 87, 189, 232), // cor de fundo da splash
->>>>>>> 5ebf68495b0941f9edbb0c59300078ccac5619a1
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
